f5f0d89e9b2574beb34fc7265999fa9597bc53cc
PhuDang-Icetea <phu.dang@icetea.io>
2024-03-08 14:38:50 +0700

    add update data renderer

--- assets/Scripts/Utils/Split/AssemblerSplit.ts
+++ assets/Scripts/Utils/Split/AssemblerSplit.ts
@@ -1,10 +1,159 @@
-import { _decorator, Component, IAssembler, Node } from 'cc';
+import * as SplitHelper from "./Helper";
+import { _decorator, Component, IAssembler, Mat4, Node, RenderData, Vec2 } from 'cc';
+import { SplitRender } from "./SplitRender";
 const { property } = _decorator;
 
-export namespace Utils.Split {
-    export class AssemblerSplit implements IAssembler {
+// export namespace Utils.Split {
+export class AssemblerSplit implements IAssembler {
 
-        // createData(com: SplitRen);
+    createData(com: SplitRender): RenderData {
+        let vertexCount = 4;
+        let indexCount = 6;
+
+        const renderData: RenderData = com.requestRenderData();
+        renderData.dataLength = vertexCount;
+        renderData.resize(vertexCount, indexCount);
+
+        return renderData;
+    }
+
+    resetData(com: SplitRender) {
+        let points: Vec2[] = com.polygon;
+        if (!points || points.length < 3) return;
+
+        let vertexCount: number = points.length;
+        let indexCount: number = vertexCount + (vertexCount - 3) * 2;
+
+        com.renderData.clear();
+        com.renderData.dataLength = vertexCount;
+        com.renderData.resize(vertexCount, indexCount);
+
+        let material = com.renderData.material;
+        com.renderData.material = material;
+    }
+
+    updateRenderData(com: SplitRender) {
+        const renderData: RenderData = com.renderData;
+        if (renderData.vertDirty) {
+            this.resetData(com);
+            this.updateVertexData(com);
+            this.updateUvs(com);
+            this.updateColor(com);
+            renderData.updateRenderData(com, com.spriteFrame);
+        }
+    }
+
+    updateWorldVerts(com: SplitRender, verts: Float32Array) {
+        let floatsPerVert = 9;
+
+        let matrix: Mat4 = com.node.worldMatrix;
+        let a = matrix.m00;
+        let b = matrix.m01;
+        let c = matrix.m04;
+        let d = matrix.m05;
+        let tx = matrix.m12;
+        let ty = matrix.m13;
+
+        let justTranslate = a === 1 && b === 0 && c === 0 && d === 1;
+        if (justTranslate) {
+            let polygon = com.polygon;
+            for (let i = 0; i < polygon.length; i++) {
+                verts[i * floatsPerVert] = polygon[i].x + tx;
+                verts[i * floatsPerVert + 1] = polygon[i].y + ty;
+            }
+        } else {
+            let polygon = com.polygon;
+            for (let i = 0; i < polygon.length; i++) {
+                verts[i * floatsPerVert] = a * polygon[i].x + c * polygon[i].y + tx;
+                verts[i * floatsPerVert + 1] = b * polygon[i].x + d * polygon[i].y + ty;
+            }
+        }
+
+        // @ts-ignore
+        com.node._uiProps.uiTransformDirty = false;
+    }
+
+    fillBuffers(com: SplitRender, renderer: any) {
+        const chunk = com.renderData.chunk;
+        // indices generated
+        let indicesArr = SplitHelper.SplitPolygon(com.polygon);
+        this.updateWorldVerts(com, chunk.vb);
+
+        // quick version
+        const bid = chunk.bufferId;
+        const vid = chunk.vertexOffset;
+        const meshBuffer = chunk.vertexAccessor.getMeshBuffer(bid);
+        const ib = chunk.vertexAccessor.getIndexBuffer(bid);
+        let indexOffset = meshBuffer.indexOffset;
+
+        // fill indices
+        for (let i = 0, l = indicesArr.length; i < l; i++) {
+            ib[indexOffset++] = vid + indicesArr[i];
+        }
+        meshBuffer.indexOffset += indicesArr.length;
+    }
+
+    updateVertexData(com: SplitRender) {
+        const renderData = com.renderData;
+        if (!renderData) {
+            return;
+        }
+        const dataList = renderData.data;
+
+        let polygon = com.polygon;
+        for (let i = 0; i < polygon.length; i++) {
+            dataList[i].x = polygon[i].x;
+            dataList[i].y = polygon[i].y;
+        }
+
+        const chunk = com.renderData.chunk;
+        const vid = chunk.vertexOffset;
+        const ib = chunk.ib as any;
+
+        let indicesArr = SplitHelper.SplitPolygon(com.polygon);
+        for (let i = 0, l = indicesArr.length; i < l; i++) {
+            ib[i] = vid + indicesArr[i];
+        }
+    }
+
+    updateUvs(com: SplitRender) {
+        let uvOffset = 3, floatsPerVert = 9;
+        const vData = com.renderData.chunk.vb;
+
+        let uvs = [];
+        if (com.spriteFrame.texture) {
+            uvs = SplitHelper.ComputeUV(com.polygon, com.spriteFrame.texture.width, com.spriteFrame.texture.height);
+        }
+
+        let polygon = com.polygon;
+        for (let i = 0; i < polygon.length; i++) {
+            vData[uvOffset] = uvs[i].x;
+            vData[uvOffset + 1] = uvs[i].y;
+            uvOffset += floatsPerVert;
+        }
+    }
+
+    updateColor(com: SplitRender) {
+        const renderData = com.renderData!;
+
+        let colorOffset = 5, floatsPerVert = renderData.floatStride;
+        let vData = renderData.chunk.vb;
+
+        const color = com.color;
+        const colorR = color.r / 255;
+        const colorG = color.g / 255;
+        const colorB = color.b / 255;
+        const colorA = color.a / 255;
+
+        let polygon = com.polygon;
+        for (let i = 0; i < polygon.length; i++) {
+            vData![colorOffset] = colorR;
+            vData![colorOffset + 1] = colorG;
+            vData![colorOffset + 2] = colorB;
+            vData![colorOffset + 3] = colorA;
+            colorOffset += floatsPerVert;
+        }
     }
 }
+// }
 
--- assets/Scripts/Utils/Split/SplitRender.ts
+++ assets/Scripts/Utils/Split/SplitRender.ts
@@ -1,4 +1,6 @@
-import { _decorator, ccenum, Component, Node, Renderable2D, SpriteFrame } from 'cc';
+import { __private, _decorator, ccenum, Component, IAssembler, Mat4, Node, Renderable2D, RichText, SpriteFrame, Vec2 } from 'cc';
+import * as SplitHelper from './Helper';
+import { AssemblerSplit } from './AssemblerSplit';
 const { ccclass, property, executeInEditMode } = _decorator;
 
 enum TextureType {
@@ -6,6 +8,11 @@ enum TextureType {
     Stretch
 }
 ccenum(TextureType);
+
+let _vec2_temp = new Vec2();
+let _mat4_temp = new Mat4();
+
+// namespace Utils_Split {
 
 @ccclass('SplitRender')
 @executeInEditMode
@@ -16,11 +23,22 @@ export class SplitRender extends Renderable2D {
     @property({ type: SpriteFrame, serializable: true })
     protected _spriteFrame: SpriteFrame | null = null;
 
+    @property({ type: [Vec2], serializable: true })
+    _polygon: Vec2[] = [];
+
+    @property({ type: [Vec2], serializable: true })
+    public get polygon(): Vec2[] {
+        return this._polygon;
+    }
+    public set polygon(points: Vec2[]) {
+        this._polygon = points;
+        this.markForUpdateRenderData();
+    }
+
     @property({ type: SpriteFrame, serializable: true })
     get spriteFrame() {
         return this._spriteFrame;
     }
-
     set spriteFrame(value: SpriteFrame) {
         if (!value || this._spriteFrame === value) {
             return;
@@ -32,7 +50,117 @@ export class SplitRender extends Renderable2D {
         let b = -value.height / 2;
         let t = value.height / 2;
         let r = value.width / 2;
+
+        this.markForUpdateRenderData();
+        //TODO apply sprite Size
+    }
+
+    @property({ type: TextureType, serializable: true })
+    _type: TextureType = 0;
+    @property({ type: TextureType, serializable: true })
+    get type() {
+        return this._type;
+    }
+    set type(val: TextureType) {
+        this._type = val;
+        this.markForUpdateRenderData();
+    }
+
+    @property
+    editing: boolean = false;
+
+    protected _assembler: IAssembler = null;
+
+    constructor() {
+        super();
+    }
+
+    onLoad(): void {
+
+    }
+
+    protected start(): void {
+        console.log(this.node.uuid);
+
+    }
+
+    _hitTest(cameraPoint: Vec2): boolean {
+        let node: Node = this.node;
+        let testPoint: Vec2 = _vec2_temp;
+
+        node.updateWorldTransform();
+        // If scale is 0, it can't be hit.
+        if (!Mat4.invert(_mat4_temp, node.worldMatrix)) {
+            return false;
+        }
+
+        Vec2.transformMat4(testPoint, cameraPoint, _mat4_temp);
+        return SplitHelper.IsInPolygon(testPoint, this.polygon);
+    }
+
+    private _applySpriteSize() {
+        if (this._spriteFrame) {
+            const size = this._spriteFrame.originalSize;
+            this.node._uiProps.uiTransformComp.setContentSize(size);
+        }
+
+        this._activateMaterial();
+    }
+
+    private _activateMaterial() {
+        const spriteFrame = this._spriteFrame;
+        const material = this.getRenderMaterial(0);
+        if (spriteFrame && material) {
+            this.markForUpdateRenderData();
+        }
+
+        if (this.renderData) {
+            this.renderData.material = material;
+        }
+    }
+
+    protected _render(render: __private._cocos_2d_renderer_i_batcher__IBatcher): void {
+        render.commitComp(this, this.renderData, this._spriteFrame, this._assembler, null);
+    }
+
+    protected _canRender(): boolean {
+        if (!super._canRender()) {
+            return false;
+        }
+
+        const spriteFrame = this._spriteFrame;
+        if (!spriteFrame || !spriteFrame.texture) {
+            return false;
+        }
+
+        return true;
+    }
+
+    protected _flushAssembler(): void {
+        if (this._assembler == null) {
+            this.destroyRenderData();
+            this._assembler = new AssemblerSplit();
+        }
+
+        if (!this._renderData) {
+            if (this._assembler && this._assembler.createData) {
+                this._renderData = this._assembler.createData(this);
+                this.renderData!.material = this.getRenderMaterial(0);
+                this.markForUpdateRenderData();
+                this._updateColor();
+            }
+        }
+    }
+
+    protected updateMaterial(): void {
+        if (this._customMaterial) {
+            this.setMaterial(this._customMaterial, 0);
+            return;
+        }
+
+        const mat = this._updateBuiltinMaterial();
+        this.setMaterial(mat, 0);
+        this._updateBlendFunc();
     }
 }
-
-
+// }
